cad2b68334e40f775687bfa98d4a11ec1b542dd1
Firas <firassoekhai@hotmail.nl>
2020-10-27 16:15:37 +0100

    tot video 3

--- js/boeken.js
+++ js/boeken.js
@@ -4,8 +4,21 @@ const xhr = new XMLHttpRequest();
 xhr.onreadystatechange = () => {
     if(xhr.readyState == 4 && xhr.status ==200) {
         let resultaat = JSON.parse(xhr.responseText);
-        console.log(resultaat);
+        boeken.data = resultaat;
+        boeken.uitvoeren();
     } 
 }
 xhr.open('GET', 'boek.json', true);
 xhr.send();
+
+const boeken = {
+
+    // er wordt hier een eigenschop data aangemaakt (regel 7)
+    uitvoeren() {
+        let html = "";
+        this.data.forEach( boek => {
+            html += `<h3>${boek.titel}</h3>`
+        });
+        uitvoer.innerHTML = html
+    }
+}
